046f0b4afa311f0b7f584e5b677b0ab1ff8ea876
Tomer Gelbhart <gelbharttomer@gmail.com>
2026-07-26 15:50:22 +0100

    refactor(tools): nest shared tool UI modules (#237)

---
+++ src/components/tools/shared/controls/SegmentedControl.tsx
@@ -0,0 +1,19 @@
+import { HudSegmentControl } from "../../../ui/HudSegmentControl";
+
+interface SegmentedOption<Value extends string> {
+  value: Value;
+  label: string;
+}
+
+interface SegmentedControlProps<Value extends string> {
+  value: Value;
+  options: readonly SegmentedOption<Value>[];
+  onChange: (value: Value) => void;
+  "aria-label"?: string;
+}
+
+export function SegmentedControl<Value extends string>(
+  props: SegmentedControlProps<Value>,
+) {
+  return <HudSegmentControl tone="action" {...props} />;
+}
