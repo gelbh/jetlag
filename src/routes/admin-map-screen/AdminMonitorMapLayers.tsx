3793b2a0d528da45bad24be8e4b6a93207b8cd3d
gelbh <gelbharttomer@gmail.com>
2026-07-29 06:49:59 +0100

    fix(map): retarget admin monitor map imports after shim removal

--- src/routes/admin-map-screen/AdminMonitorMapLayers.tsx
+++ src/routes/admin-map-screen/AdminMonitorMapLayers.tsx
@@ -1,6 +1,6 @@
-import { GameAreaMask } from "../../components/map/GameAreaMask";
-import { MapView } from "../../components/map/MapView";
-import { MapViewportTracker } from "../../components/map/MapViewportTracker";
+import { GameAreaMask } from "../../components/map/layers/GameAreaMask";
+import { MapView } from "../../components/map/chrome/MapView";
+import { MapViewportTracker } from "../../components/map/chrome/MapViewportTracker";
 import { fallbackGameArea } from "../../domain/geometry/core/gameAreaConvert";
 import { AdminMonitorPlayerFocus } from "../../components/admin/AdminMonitorPlayerFocus";
 import type { ObserverMapScreenController } from "../observer-map-screen/useObserverMapScreen";
